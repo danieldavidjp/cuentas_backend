1dfad683e06100ea41379a3f9019e29a0016d8f2
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-28 10:32:46 -0500

    Se agregan parametros adicionales al sp g1_sp_cuenta_corriente.sql

--- SP's/g1_sp_cuenta_corriente.sql
+++ SP's/g1_sp_cuenta_corriente.sql
@@ -1,6 +1,23 @@
 USE cobis
 GO
 CREATE PROCEDURE g1_sp_cuenta_corriente (
+	@s_ssn			int = NULL,
+	@s_user			login = NULL,
+	@s_sesn			int = NULL,
+	@s_term			varchar(32) = NULL,
+	@s_date			datetime = NULL,
+	@s_srv			varchar(30) = NULL,
+	@s_lsrv			varchar(30) = NULL, 
+	@s_rol			smallint = NULL,
+	@s_ofi			smallint = NULL,
+	@s_org_err		char(1) = NULL,
+	@s_error		int = NULL,
+	@s_sev			tinyint = NULL,
+	@s_msg			descripcion = NULL,
+	@s_org			char(1) = NULL,
+	@t_debug		char(1) = 'N',
+	@t_file			varchar(14) = null,
+	@t_from			varchar(32) = null,
 	@t_trn				int			= 1,
 	@i_saldo			float		= 50,
 	@i_numero_cuenta	varchar(10),
@@ -20,6 +37,8 @@ AS
 		IF @@ROWCOUNT > 0
 		BEGIN
 			exec cobis..sp_cerror
+						@t_debug  = @t_debug,
+                        @t_file   = @t_file,
                         @t_from  = @w_sp_name,
                         @i_num   = 105001 -- TODO: crear codigo de error en la bd
                 return 1
